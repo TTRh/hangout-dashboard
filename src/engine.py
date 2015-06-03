98f09fca1af6517f9bd4694ac7a5f4b886515f97
pierre <pierre.houssin@yahoo.fr>
2015-06-04 01:51:58 +0200

    refactor some functions

--- src/engine.py
+++ src/engine.py
@@ -7,6 +7,7 @@ import itertools
 import re
 from datetime import timedelta
 from collections import *
+import json
 
 from context import *
 
@@ -30,6 +31,28 @@ def init_counter(counter,keyfunc=lambda k:k,valfunc=lambda x,y:x+y,default_type=
             result[keyfunc(k)] = val
     return result
 
+def mean(iterable):
+    n,total = 0,0
+    for v in iterable:
+        n+=1
+        total += v
+    if n == 0:
+        return None
+    else:
+        return float(total)/n
+
+def median(lst):
+    n = len(lst)
+    if n == 0:
+        return None
+    elif n % 2 == 0:
+        return float(lst[n/2-1] + lst[n/2])/2
+    else:
+        return lst[n/2]
+
+def strtime2seconds(strtime):
+    return int(strtime[:2])*3600 + int(strtime[2:5])*60 + int(strtime[-2:])
+
 #####################
 # statistic engine
 #####################
@@ -37,115 +60,131 @@ def init_counter(counter,keyfunc=lambda k:k,valfunc=lambda x,y:x+y,default_type=
 class HangoutStatistic:
 
     def __init__(self,hangout,conversation_id):
+        # input param
         self.hangout = hangout
         self.conversation = hangout.get_conversation(conversation_id)
+        # private variables
         self.user_statistics_accumulator = {}
         self.user_statistics = {}
+        self.global_statistics = {}
 
     def init_user_statistics(self,user_id):
-        sa = {}
-        s = {}
-        # structures
-        sa["events_vs_datetime"] = Counter()
-        # general info
-        s["name"] = self.conversation.get_participant(user_id).get_name()
-        # global counters
-        s["sum_events"] = None # total number of events
-        s["sum_links"] = None # total numbers of links
-        s["sum_words"] = None # total number of words
-        s["sum_uniq_words"] = None # total number of unique words (vocabulary)
-        s["sum_reference"] = None # total number of user reference
-        # text analysis
-        s["main_words"] = None # main words
-        s["main_site"] = None # main site redirection
-        s["aliases"] = None # list of alias
-        s["quotes"] = None # list of quote
-        s["longest_word"] = None # longuest words posted
-        s["longest_post"] = None # longuest message text posted
-        # Ym vs events
-        s["sparkline_sum_events_vs_month"] = None # number of event per month
-        # Ymd vs events
-        s["avg_day_events"] = None # avg number of event by day
-        s["avg_day_links"] = None # avg number of links by day
-        s["sum_days_with_event"] = None # total days with at least one event
-        s["max_day_events"] = None # max event in one day
-        s["sparkline_sum_events_vs_day"] = None # number of event per day
-        # YmdH vs events
-        s["max_hour_events"] = None # max frequency event posted in one hour
-        # YmdHM vs events
-        s["sparkline_avg_events_vs_time"] = None # avg number of event per time in a day
-        # HMS vs events
-        s["max_time_event"] = None 
-        s["min_time_event"] = None 
-        # Ymd vs HMS
-        s["avg_max_time_event"] = None 
-        s["avg_min_time_event"] = None 
-        # initialization
-        self.user_statistics[user_id] = s
-        self.user_statistics_accumulator[user_id] = sa
+        # init accumulators
+        acc = {
+            "event_per_Ym" : Counter(),
+            "event_per_Ymd" : Counter(),
+            "event_per_YmdH" : Counter(),
+            "event_per_YmdHM" : Counter(),
+            "event_per_YmdHMS" : Counter(),
+            "HMS_per_Ymd" : defaultdict(list)
+        }
+        self.user_statistics_accumulator[user_id] = acc
+        # init statistics
+        stats = [
+            "name", # username
+            "sum_events", # total number of events
+            "sum_links", # total numbers of links
+            "sum_words", # total number of words
+            "sum_uniq_words", # total number of unique words (vocabulary)
+            "sum_reference", # total number of user reference
+            # text analysis
+            "main_words", # main words
+            "main_site", # main site redirection
+            "aliases", # list of alias
+            "quotes", # list of quote
+            "longest_word", # longuest words posted
+            "longest_post", # longuest message text posted
+            # Ym vs events
+            "sparkline_sum_events_vs_month", # number of event per month
+            # Ymd vs events
+            "avg_day_events", # avg number of event by day
+            "avg_day_links", # avg number of links by day
+            "sum_days_with_event", # total days with at least one event
+            "max_day_events", # max event in one day
+            "sparkline_sum_events_vs_day", # number of event per day
+            # YmdH vs events
+            "max_hour_events", # max frequency event posted in one hour
+            # YmdHM vs events
+            "sparkline_avg_events_vs_time", # avg number of event per time in a day
+            # Ymd vs HMS
+            "max_time_event", 
+            "min_time_event", 
+            "avg_max_time_event", 
+            "avg_min_time_event", 
+        ]
+        self.user_statistics[user_id] = dict.fromkeys(stats)
 
     def update_user_statistics(self,user_id):
-        sa = self.user_statistics_accumulator[user_id]
-        s = self.user_statistics[user_id]
-        # structure
-        c_datetime = sa["events_vs_datetime"]
-        if len(c_datetime) == 0:
+        # init local variable
+        acc = self.user_statistics_accumulator[user_id]
+        stats = self.user_statistics[user_id]
+        all_HM = map(lambda x:(3-len(x))*'0' + x,[ str(i*10 + j) for i in xrange(0,24) for j in xrange(0,6) ])
+        # TODO : deal spy users
+        if len(acc["event_per_YmdHMS"]) == 0:
             return
-        # global counters
-        s["sum_events"] = sum(c_datetime.itervalues())
-        # Ym vs events
-        c_datetime_Ym = init_counter(c_datetime,lambda k:k[:6])
-        # s["sparkline_sum_events_vs_month"] = [ c_datetime_Ym[k] for k in all_months ]
-        # Ymd vs events
-        c_datetime_Ymd = init_counter(c_datetime,lambda k:k[:8])
-        s["avg_day_events"] = sum(c_datetime_Ymd.itervalues())/len(c_datetime_Ymd)
-        s["sum_days_with_event"] = len(c_datetime_Ymd)
-        s["max_day_events"] = max(c_datetime_Ymd.itervalues())
-        # s["sparkline_sum_events_vs_day"] = [ c_datetime_Ymd[k] for k in all_days ]
-        # YmdH vs events
-        c_datetime_YmdH = init_counter(c_datetime,lambda k:k[:-4])
-        s["max_hour_events"] = max(c_datetime_YmdH.itervalues())
-        # YmdHM vs events
-        c_datetime_YmdHM = init_counter(c_datetime,lambda k:k[:-2])
-        c_datetime_HM = init_counter(c_datetime_YmdHM,lambda k:k[-4:],lambda x,y:x.append(y),list)
-        # s["sparkline_avg_events_vs_time"] = [ sum(c_datetime_HM[k])/len(c_datetime_HM[k]) if len(c_datetime_HM[k])>0 else 0 for k in all_times ]
-        # HMS vs events
-        c_datetime_HMS = init_counter(c_datetime,lambda k:k[-6:])
-        s["max_time_event"] = max(c_datetime_HMS.iterkeys())
-        s["min_time_event"] = min(c_datetime_HMS.iterkeys())
-        # Ymd vs HMS
-        c_datetime_Ymd_vs_HMS = defaultdict(list)
-        for k in c_datetime.iterkeys():
-            c_datetime_Ymd_vs_HMS[k[:8]].append(k[:-8])
-        max_HMS = map(lambda x:max(x),c_datetime_Ymd_vs_HMS.itervalues())
-        max_HMS = map(lambda x:int(x[:2])*3600+int(x[3:5])*60+int(x[:-2]),max_HMS)
-        max_S = sum(max_HMS)/len(max_HMS)
-        s["avg_max_time_event"] = str(timedelta(seconds=max_S))
+        # update statistics
+        stats["sum_events"] = sum(acc["event_per_YmdHMS"].itervalues())
+        stats["sum_links"] = None
+        stats["sum_words"] = None
+        stats["sum_uniq_words"] = None
+        stats["sum_reference"] = None
+        stats["main_words"] = None
+        stats["main_site"] = None
+        stats["aliases"] = None
+        stats["quotes"] = None
+        stats["longest_word"] = None
+        stats["longest_post"] = None
+        stats["sparkline_sum_events_vs_month"] = None # [ acc["event_per_Ym"][k] for k in all_Ym ]
+        stats["avg_day_events"] = mean(acc["event_per_Ymd"].values())
+        stats["avg_day_links"] = None
+        stats["sum_days_with_event"] = len(acc["event_per_Ymd"])
+        stats["max_day_events"] = max(acc["event_per_Ymd"].itervalues())
+        stats["sparkline_sum_events_vs_day"] = None # [ acc["event_per_Ymd"][k] for k in all_Ymd ]
+        stats["max_hour_events"] = max(acc["event_per_YmdH"].itervalues())
+        acc_event_per_HM = init_counter(acc["event_per_YmdHM"],lambda k:k[-3:],lambda x,y:x.append(y),list)
+        stats["sparkline_avg_events_vs_time"] = [ (k,sum(acc_event_per_HM[k])) if len(acc_event_per_HM[k]) > 0 else (k, 0) for k in all_HM ]
+        stats["max_time_event"] = max(reduce(lambda x,y:x+y,acc["HMS_per_Ymd"].itervalues()))
+        stats["min_time_event"] = min(reduce(lambda x,y:x+y,acc["HMS_per_Ymd"].itervalues()))
+        stats["avg_max_time_event"] = str(timedelta(seconds = median(map(strtime2seconds,[max(l) for l in acc["HMS_per_Ymd"].itervalues()]))))
+        stats["avg_min_time_event"] = str(timedelta(seconds = median(map(strtime2seconds,[min(l) for l in acc["HMS_per_Ymd"].itervalues()]))))
 
-        min_HMS = map(lambda x:min(x),c_datetime_Ymd_vs_HMS.itervalues())
-        min_HMS = map(lambda x:int(x[:2])*3600+int(x[3:5])*60+int(x[:-2]),min_HMS)
-        min_S = sum(min_HMS)/len(min_HMS)
-        s["avg_min_time_event"] = str(timedelta(seconds=max_S))
+    def init_global_statistics(self):
+        stats = [
+            "max_YM",
+            "min_YM",
+            "sum_events"
+        ]
+        self.global_statistics = dict.fromkeys(stats)
 
     def init(self):
+        # init global statistics
+        self.init_global_statistics()
         # init users statistics
         for p in self.conversation.iter_participant():
             self.init_user_statistics(p.get_id())
+            self.user_statistics[p.get_id()]["name"] = p.get_name()
 
     def optimize(self):
         for e in self.conversation.iter_event():
-            # deal unknow users
+            # TODO : deal unknow users
             if e.get_sender() not in self.user_statistics_accumulator:
                 continue
-            # get user stats object
-            sa = self.user_statistics_accumulator[e.get_sender()]
             # local variable helper
-            datetime = e.get_datetime()
+            acc = self.user_statistics_accumulator[e.get_sender()]
             text = e.get_text()
-            # fill structures
-            sa["events_vs_datetime"][datetime] += 1
-            # compute stats
-            # s["total_links"] += len(RE_URL.findall(text))
+            dt = e.get_datetime()
+            dt_Ym = dt[:6]
+            dt_Ymd = dt[:8]
+            dt_YmdH = dt[:-4]
+            dt_YmdHM = dt[:-3]
+            dt_HMS = dt[-6:]
+            # update accumulators
+            acc["event_per_Ym"][dt_Ym] += 1
+            acc["event_per_Ymd"][dt_Ymd] += 1
+            acc["event_per_YmdH"][dt_YmdH] += 1
+            acc["event_per_YmdHM"][dt_YmdHM] += 1
+            acc["HMS_per_Ymd"][dt_Ymd].append(dt_HMS)
+            acc["event_per_YmdHMS"][dt] += 1
 
     def update(self):
         for u in self.user_statistics.iterkeys():
@@ -155,26 +194,4 @@ class HangoutStatistic:
         self.init()
         self.optimize()
         self.update()
-        print self.user_statistics["100004041546029582490"]
-
-if __name__ == '__main__':
-
-    test = Counter()
-    test["201504"] += 1
-    test["201404"] += 1
-    test["201405"] += 1
-
-    print "test:"
-    print test
-
-    print "identity:"
-    test1 = init_counter(test)
-    print test1
-
-    print "year:"
-    test2 = init_counter(test,lambda k:k[:4])
-    print test2
-
-    print "avg month:"
-    test3 = init_counter(test,lambda k:k[-2:],lambda x,y:x.append(y),list)
-    print test3
+        print json.dumps(self.user_statistics["100004041546029582490"])
