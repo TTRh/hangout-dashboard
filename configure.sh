739e1a044263d4488a62e7cf66990d0efd717469
pierre <pierre.houssin@yahoo.fr>
2015-07-21 02:07:17 +0200

    bugfix

--- configure.sh
+++ configure.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
-BOWER_DIR=$SCRIPT_DIR/web/public
+BOWER_DIR=$SCRIPT_DIR/web/public/bower_components
 
 function check_bower_dep {
-  test -d $1 || echo "[WARNING] run 'bower install $1' in $BOWER_DIR"
+  test -d $1 && echo "[INFO] $1 OK" || echo "[WARNING] run 'bower install $1' in $(dirname $BOWER_DIR)"
 }
 
-echo "[INFO] : check bower compoments ..."
-pushd $BOWER_DIR
+echo "[INFO] : check web compoments ..."
+mkdir -p $BOWER_DIR
+pushd $BOWER_DIR >/dev/null
 check_bower_dep jquery
-check_bower_dep fontawsome
+check_bower_dep fontawesome
 check_bower_dep bootstrap
 check_bower_dep ihover
 check_bower_dep jquery.sparkline
-popd
+popd >/dev/null
